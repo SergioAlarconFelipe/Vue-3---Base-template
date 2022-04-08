ee290c5262b1e69837fffb632e55549d1b7df711
Sergio Alarcón Felipe <sergio.alarcon.felipe@gmail.com>
2022-04-08 10:42:12 +0200

    Update webpack.config.base.js

--- build/webpack.config.base.js
+++ build/webpack.config.base.js
@@ -1,100 +1,106 @@
 'use strict'
 
-const HtmlWebpackPlugin = require('html-webpack-plugin')
-const CopyWebpackPlugin = require('copy-webpack-plugin')
-const { VueLoaderPlugin } = require('vue-loader')
-const webpack = require('webpack')
+const HtmlWebpackPlugin = require( 'html-webpack-plugin' )
+const CopyWebpackPlugin = require( 'copy-webpack-plugin' )
+const { VueLoaderPlugin } = require( 'vue-loader')
+const webpack = require( 'webpack' )
 
-const utils = require('./utils')
+const utils = require( './utils' )
 
 module.exports = {
-  resolve: {
-    extensions: ['.js', '.vue', '.json'],
-    alias: {
-      'assets': utils.resolve('assets'),
-      'pages': utils.resolve('src/pages'),
-      'static': utils.resolve('static'),
-      'components': utils.resolve('src/components')
-    }
-  },
+	resolve: {
+		extensions: [ '.js', '.vue', '.json' ],
+		alias: {
+			'assets': utils.resolve( 'assets' ),
+			'pages': utils.resolve( 'src/pages' ),
+			'static': utils.resolve( 'static' ),
+			'components': utils.resolve( 'src/components' )
+		}
+	},
 
-  module: {
-    rules: [
-      {
-        test: /\.(js|vue)$/,
-        use: 'eslint-loader',
-        enforce: 'pre'
-      }, {
-        test: /\.vue$/,
-        use: 'vue-loader'
-      }, {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-        }
-      }, {
-        test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
-        use: {
-          loader: 'url-loader',
-          options: {
-            limit: 10000,
-            name: utils.assetsPath('img/[name].[hash:7].[ext]')
-          }
-        }
-      }, {
-        test: /\.(mp4|webm|ogg|mp3|wav|flac|aac)(\?.*)?$/,
-        use: {
-          loader: 'url-loader',
-          options: {
-            limit: 10000,
-            name: utils.assetsPath('media/[name].[hash:7].[ext]')
-          }
-        }
-      }, {
-        test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
-        use: {
-          loader: 'url-loader',
-          options: {
-            limit: 10000,
-            name: utils.assetsPath('fonts/[name].[hash:7].[ext]')
-          }
-        }
-      }, {
-        test: /\.scss$/,
-        use: [
-          'vue-style-loader',
-          'css-loader',
-          {
-            loader: "sass-loader",
-            options: {
-              // Prefer `dart-sass`
-              implementation: require("sass"),
-              // additionalData: '@import "./assets/css/global.scss"; @import "./assets/css/theme-dark.scss";',
-            },
-          },
-        ]
-      }
-    ]
-  },
+	module: {
+		rules: [
+			{
+				test: /\.(js|vue)$/,
+				use: 'eslint-loader',
+				enforce: 'pre'
+			}, 
+			{
+				test: /\.vue$/,
+				use: 'vue-loader'
+			},
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				use: {
+					loader: 'babel-loader',
+				}
+			}, 
+			{
+				test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
+				use: {
+					loader: 'url-loader',
+					options: {
+						limit: 10000,
+						name: utils.assetsPath( 'img/[name].[hash:7].[ext]' )
+					}
+				}
+			}, 
+			{
+				test: /\.(mp4|webm|ogg|mp3|wav|flac|aac)(\?.*)?$/,
+				use: {
+					loader: 'url-loader',
+					options: {
+						limit: 10000,
+						name: utils.assetsPath( 'media/[name].[hash:7].[ext]' )
+					}
+				}
+			}, 
+			{
+				test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
+				use: {
+					loader: 'url-loader',
+					options: {
+						limit: 10000,
+						name: utils.assetsPath( 'fonts/[name].[hash:7].[ext]' )
+					}
+				}
+			}, 
+			{
+				test: /\.scss$/,
+				use: [
+					'vue-style-loader',
+					'css-loader',
+					{
+						loader: "sass-loader",
+						options: {
+							// Prefer `dart-sass`
+							implementation: require( 'sass' ),
+							// additionalData: '@import "./assets/css/global.scss"; @import "./assets/css/theme-dark.scss";',
+						},
+					},
+				]
+			}
+		]
+	},
 
-  plugins: [
-    new HtmlWebpackPlugin({
-      filename: 'index.html',
-      template: 'index.html',
-      inject: true
-    }),
-    new VueLoaderPlugin(),
-    new CopyWebpackPlugin({
-      patterns: [{
-        from: utils.resolve('static/img'),
-        to: utils.resolve('dist/static/img'),
-        toType: 'dir'
-      }]
-    }),
-    new webpack.DefinePlugin({
-      __VUE_OPTIONS_API__: true,
-      __VUE_PROD_DEVTOOLS__: false,
-    }),
-  ]
+	plugins: [
+		new HtmlWebpackPlugin( {
+			filename: 'index.html',
+			template: 'index.html',
+			inject: true
+		} ),
+		new VueLoaderPlugin(),
+		new CopyWebpackPlugin( {
+			patterns: [ {
+				from: utils.resolve( 'static' ),
+				to: utils.resolve( 'dist/static' ),
+				toType: 'dir'
+			} ]
+		} ),
+		new webpack.DefinePlugin( {
+			__VUE_OPTIONS_API__: true,
+			__VUE_PROD_DEVTOOLS__: false,
+		} ),
+	]
 }
