93e839e0e37b7e64a93f3d7572f64a843ae14f89
Sergio Alarcón Felipe <sergio.alarcon.felipe@gmail.com>
2022-04-08 10:43:34 +0200

    Update webpack.config.prod.js

--- build/webpack.config.prod.js
+++ build/webpack.config.prod.js
@@ -1,46 +1,47 @@
 'use strict'
 
-const { merge } = require('webpack-merge')
-const baseConfig = require('./webpack.config.base')
-const MiniCssExtractPlugin  = require('mini-css-extract-plugin')
+const { merge } = require( 'webpack-merge' )
+const baseConfig = require( './webpack.config.base' )
+const MiniCssExtractPlugin  = require( 'mini-css-extract-plugin' )
 
-module.exports = merge(baseConfig, {
-  mode: 'production',
+module.exports = merge( baseConfig, {
+	mode: 'production',
 
-  optimization: {
-    splitChunks: {
-      cacheGroups: {
-        commons: {
-          test: /[\\/]node_modules[\\/]/,
-          name: "vendor",
-          chunks: "all",
-        },
-      },
-    },
-  },
+	optimization: {
+		splitChunks: {
+			cacheGroups: {
+			commons: {
+				test: /[\\/]node_modules[\\/]/,
+				name: "vendor",
+				chunks: "all",
+			},
+			},
+		},
+	},
 
-  module: {
-    rules: [
-      {
-        test: /\.css?$/,
-        use: [
-          MiniCssExtractPlugin.loader,
-          'css-loader'
-        ]
-      }, {
-        test: /\.styl(us)?$/,
-        use: [
-          MiniCssExtractPlugin.loader,
-          'css-loader',
-          'stylus-loader'
-        ]
-      }
-    ]
-  },
-  
-  plugins: [
-    new MiniCssExtractPlugin({
-      filename: 'main.css'
-    })
-  ]
-})
+	module: {
+		rules: [
+			{
+				test: /\.css?$/,
+				use: [
+					MiniCssExtractPlugin.loader,
+					'css-loader'
+				]
+			}, 
+			{
+				test: /\.styl(us)?$/,
+				use: [
+					MiniCssExtractPlugin.loader,
+					'css-loader',
+					'stylus-loader'
+				]
+			}
+		]
+	},
+
+	plugins: [
+		new MiniCssExtractPlugin( {
+			filename: 'main.css'
+		} )
+	]
+} )
